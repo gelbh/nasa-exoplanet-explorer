3051f39b46a600eb97f90425d3e825f5c59243b5
gelbh <gelbharttomer@gmail.com>
2025-10-28 15:07:33 +0000

    fix: correct Bootstrap JS import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'boxicons/css/boxicons.min.css'
 import './index.css'
 import App from './App.jsx'
+
+// Import Bootstrap JS - needed for tabs, modals, etc.
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
